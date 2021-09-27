f5a56fd74fc5b4820fc064fee0134496142f1e80
Hadi M <mhn.hadi@outlook.com>
2021-09-27 10:43:13 +0330

    removed token key

--- lib/sup_base.dart
+++ lib/sup_base.dart
@@ -3,7 +3,7 @@ import 'package:supabase/supabase.dart';
 class SupBase {
   static getPosts() async {
     final client = SupabaseClient('https://inixuaheiqtqmdubnnpy.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDYzOTI5MiwiZXhwIjoxOTQ2MjE1MjkyfQ.2IrKmKz49DkKKXxrYrP6Xc8KrenfxFInq_3XM4IKLVw');
+        'my-token-key');
 
     final res = await client.from('news').select().execute();
 
